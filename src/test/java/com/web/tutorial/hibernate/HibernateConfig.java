ac15f822180696d4fa2cb8ba09e3981f46549298
AliGoktas <ali.goktas@ali-MacBook-Pro.local>
2020-02-05 14:28:17 +0300

    a

--- src/test/java/com/web/tutorial/hibernate/HibernateConfig.java
+++ src/test/java/com/web/tutorial/hibernate/HibernateConfig.java
@@ -17,7 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @Configuration
 @PropertySource(value = "classpath:/database-test.properties")
-@ComponentScan(basePackages = "com.web.tutorial.hibernate")
 @ContextConfiguration(classes = HibernateConfig.class)
 public class HibernateConfig
 {
