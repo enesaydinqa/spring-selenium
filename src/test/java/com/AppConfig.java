44f79a545f374e40ae5d38e7418b3ac1152929c7
Enes Aydın <enes.aydin@sahibinden.com>
2020-02-10 18:46:43 +0300

    order

--- src/test/java/com/AppConfig.java
+++ src/test/java/com/AppConfig.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Order(0)
 @RunWith(SpringRunner.class)
 @PropertySource(value =
         {
--- src/test/java/com/web/tutorial/hibernate/HibernateConfig.java
+++ src/test/java/com/web/tutorial/hibernate/HibernateConfig.java
@@ -1,20 +1,16 @@
 package com.web.tutorial.hibernate;
 
-import javax.sql.DataSource;
-
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import javax.sql.DataSource;
+
 @Configuration
 @PropertySource(value = "classpath:/database-test.properties")
 @ContextConfiguration(classes = HibernateConfig.class)
@@ -22,7 +18,6 @@ public class HibernateConfig
 {
     @Autowired
     private Environment environment;
-
 
     @Autowired
     protected JdbcTemplate jdbcTemplate;
