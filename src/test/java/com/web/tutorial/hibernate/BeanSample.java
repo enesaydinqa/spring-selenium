b80f50b5a5a36f2200018b263ef9065cba82e5f2
AliGoktas <ali.goktas@ali-MacBook-Pro.local>
2020-02-11 15:26:09 +0300

    -

--- src/test/java/com/web/tutorial/hibernate/BeanSample.java
+++ src/test/java/com/web/tutorial/hibernate/BeanSample.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
